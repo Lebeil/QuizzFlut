a2bf583d7aa14009afb19e7ac1defcb56d4529e0
Lebeil <liedel75@gmail.com>
2021-09-23 19:39:25 +0200

    add CustomText

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzflut/widgets/custom_text.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -21,7 +22,25 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Card(
+              elevation: 10,
+              child: SizedBox(
+                height: MediaQuery.of(context).size.width * 0.8,
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Image.asset("assets/cover.jpg",
+                  fit: BoxFit.cover,
+                ),
+              )
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+              ),
+              onPressed: () {
 
+              },
+              child: CustomText('Commencer le quiz', factor: 1.5),
+            )
           ],
         ),
       ),
--- lib/widgets/my_app.dart
+++ lib/widgets/my_app.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'QuizzFlut',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Quizz Flutter'),
+      home: const MyHomePage(title: 'Quiz vrai ou faux'),
     );
   }
 }
